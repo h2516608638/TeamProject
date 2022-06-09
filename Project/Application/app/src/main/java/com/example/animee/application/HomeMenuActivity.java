aac395fd50104631f94cc25b459b7de06ce225e4
h2516608638 <2516608638@qq.com>
2022-06-09 11:50:54 +0800

    project

---
+++ Project/Application/app/src/main/java/com/example/animee/application/HomeMenuActivity.java
@@ -0,0 +1,31 @@
+package com.example.animee.application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+import com.example.animee.application.food_grid.FoodGridActivity;
+import com.example.animee.application.food_list.InfoListActivity;
+
+public class HomeMenuActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_menu);
+    }
+    public void onClick(View view) {
+        Intent intent = new Intent();
+        switch (view.getId()) {
+            case R.id.home_btn1:
+                intent.setClass(HomeMenuActivity.this, InfoListActivity.class);
+                break;
+            case R.id.home_btn2:
+                intent.setClass(HomeMenuActivity.this, FoodGridActivity.class);
+                break;
+        }
+        startActivity(intent);
+    }
+}
